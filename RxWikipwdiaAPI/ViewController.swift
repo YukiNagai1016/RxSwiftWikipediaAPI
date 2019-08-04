8e6fcffaded4a79a85f006299355b2fc16a5859c
優樹永井 <wadachi0505@gmail.com>
2019-08-04 18:26:14 +0900

    Initial Commit

---
+++ RxWikipwdiaAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxWikipwdiaAPI
+//
+//  Created by 優樹永井 on 2019/08/04.
+//  Copyright © 2019 com.nagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
